d1e7b84557a5aa014f460976f035f7c64efd9b4f
Gurvir Uppal <gurviruppal@icloud.com>
2021-04-23 09:34:54 -0400

    Add project files.

---
+++ playGameProject/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace playGameProject
+{
+    partial class back1State
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(back1State));
+            this.startText = new System.Windows.Forms.Button();
+            this.timeText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startText
+            // 
+            this.startText.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.startText.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.startText.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.startText.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startText.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startText.ForeColor = System.Drawing.SystemColors.Control;
+            this.startText.Location = new System.Drawing.Point(62, 41);
+            this.startText.Name = "startText";
+            this.startText.Size = new System.Drawing.Size(158, 42);
+            this.startText.TabIndex = 1;
+            this.startText.Text = "Play Game";
+            this.startText.UseVisualStyleBackColor = false;
+            this.startText.Click += new System.EventHandler(this.startText_Click);
+            // 
+            // timeText
+            // 
+            this.timeText.AutoSize = true;
+            this.timeText.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.timeText.Location = new System.Drawing.Point(88, 57);
+            this.timeText.Name = "timeText";
+            this.timeText.Size = new System.Drawing.Size(35, 13);
+            this.timeText.TabIndex = 2;
+            this.timeText.Text = "label1";
+            // 
+            // back1State
+            // 
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.timeText);
+            this.Controls.Add(this.startText);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "back1State";
+            this.Text = "Worlds Best Game ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startText;
+        private System.Windows.Forms.Label timeText;
+    }
+}
+
---
+++ playGameProject/Form1.cs
@@ -0,0 +1,69 @@
+﻿/* Gurvir Uppal
+ * ICS3U
+ * Mr. T
+ * April 22 2021
+ */
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms; 
+using System.Threading; //allows the use of Thread.Sleep() 
+using System.Media; //allows the use of SoundPlayer
+namespace playGameProject
+{
+    public partial class back1State : Form
+    {
+        public back1State()
+        {
+           InitializeComponent();
+            timeText.Text = ""; // The countdown text starts as blank
+        }
+
+
+
+        private void startText_Click(object sender, EventArgs e)
+        {
+            SoundPlayer bleepPlayer = new SoundPlayer(Properties.Resources.Bleep_SoundBible_com_1927126940);
+            SoundPlayer crashPlayer = new SoundPlayer(Properties.Resources.Car_Screech_And_Crash_SoundBible_com_1414562045);
+
+            startText.Hide(); // Hide the play game button
+
+            timeText.Text = "Game Starting in: 3";
+            bleepPlayer.Play();
+
+            Refresh();            //redraw the screen to show updates then pause for 1 second //redraw the screen to show updates then pause for 1 second 
+            Thread.Sleep(1000);
+
+            timeText.Text = "Game Starting in: 2";
+            bleepPlayer.Play();
+
+            Refresh();            //redraw the screen to show updates then pause for 1 second 
+            Thread.Sleep(1000);
+
+            timeText.Text = "Game Starting in: 1";
+            bleepPlayer.Play();
+
+            Refresh();           //redraw the screen to show updates then pause for 1 second 
+            Thread.Sleep(1000);
+
+            timeText.Text = "           GO!!!";
+            crashPlayer.Play();
+
+            this.BackColor = Color.Green; //change the color of the background
+           
+
+        }
+
+      
+    }
+}
